cd02642fab8d7827e50ef28589aeea6fca929129
Osman Azabagić <osman.azabagic@fet.ba>
2022-03-19 18:36:49 +0100

    server Latest

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
+app.set("view engine", "ejs")
 const port = 3000
 
-app.get('/aa', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/bb', (req, res) => {
-  res.send('bbbbb')
+app.get('/', (req, res) => {
+  res.render("index")
 })
 
 app.listen(port, () => {
