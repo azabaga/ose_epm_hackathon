ba1b522a442fcdec5180e25095cccbb315431930
Osman Azabagić <osman.azabagic@fet.ba>
2022-03-20 06:31:52 +0100

    login popup dodan - svaki salje na emina.html

--- js/main.js
+++ js/main.js
@@ -164,4 +164,25 @@
     })
   });
 
+      let modalBtns = [...document.querySelectorAll(".button")];
+      modalBtns.forEach(function (btn) {
+        btn.onclick = function () {
+          let modal = btn.getAttribute("data-modal");
+          document.getElementById(modal).style.display = "block";
+        };
+      });
+      let closeBtns = [...document.querySelectorAll(".close")];
+      closeBtns.forEach(function (btn) {
+        btn.onclick = function () {
+          let modal = btn.closest(".modal");
+          modal.style.display = "none";
+        };
+      });
+      // window.onclick = function (event) {
+      //   if (event.target.className === "") {
+      //     event.target.style.display = "";
+      //   }
+      // };
+    
+
 })()
